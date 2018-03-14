64f140f20881e1c260a05cbf3bb2142b3fa58a26
Jeron A. Lau <jeron.lau@plopgrizzly.com>
2018-03-14 12:06:33 -0500

    OpenGL stuff.

--- examples/animated/src/main.rs
+++ examples/animated/src/main.rs
@@ -17,16 +17,16 @@ const IMAGE_TEXCOORDS: &'static [f32] = &include!("../res/image.texc");
 const TRIANGLE_MODEL: &'static [f32] = &include!("../res/triangle.data");
 const TRIANGLE_INDICES: &'static [u32] = &include!("../res/triangle.indices");
 
-pub fn resize(display: &mut adi_gpu::Display, image_tex: adi_gpu::Texture,
-	logo_tex: adi_gpu::Texture) -> adi_gpu::Shape
+pub fn resize(display: &mut adi_gpu::Display, /*image_tex: adi_gpu::Texture,
+	logo_tex: adi_gpu::Texture*/) -> adi_gpu::Shape
 {
 	println!("!Resizing...");
 
-	let square_model = display.model(SQUARE_MODEL, SQUARE_INDICES);
-	let image_model = display.model(IMAGE_MODEL, SQUARE_INDICES);
+//	let square_model = display.model(SQUARE_MODEL, SQUARE_INDICES);
+//	let image_model = display.model(IMAGE_MODEL, SQUARE_INDICES);
 	let triangle_model = display.model(TRIANGLE_MODEL, TRIANGLE_INDICES);
 
-	let image_texcoords = display.texcoords(IMAGE_TEXCOORDS);
+//	let image_texcoords = display.texcoords(IMAGE_TEXCOORDS);
 
 	let gradient = display.gradient(&[
 			1.0, 0.0, 0.0, 1.0,
@@ -34,77 +34,82 @@ pub fn resize(display: &mut adi_gpu::Display, image_tex: adi_gpu::Texture,
 			0.0, 0.0, 1.0, 1.0,
 		]);
 
-	let complexity = display.gradient(&[
-			1.0, 0.0, 0.0, 1.0,
-			0.0, 0.0, 1.0, 1.0,
-			0.0, 1.0, 0.0, 1.0,
-			0.0, 1.0, 0.0, 1.0 
-		]);
+//	let complexity = display.gradient(&[
+//			1.0, 0.0, 0.0, 1.0,
+//			0.0, 0.0, 1.0, 1.0,
+//			0.0, 1.0, 0.0, 1.0,
+//			0.0, 1.0, 0.0, 1.0 
+//		]);
 
 	// square
-	display.shape_solid(&square_model, adi_gpu::Mat4::new()
-		.translate(0.0, 0.0, 0.5).rotate(0.25, 0.0, 0.0),
-		[0.5, 1.0, 0.5, 0.5], false, false, true, true);
+//	display.shape_solid(&square_model, adi_gpu::Mat4::new()
+//		.translate(0.0, 0.0, 0.5).rotate(0.25, 0.0, 0.0),
+//		[0.5, 1.0, 0.5, 0.5], false, false, true, true);
 	// image
-	display.shape_texture(&image_model, adi_gpu::Mat4::new(),
-		image_tex, image_texcoords, false, false, true, true);
+//	display.shape_texture(&image_model, adi_gpu::Mat4::new(),
+//		image_tex, image_texcoords, false, false, true, true);
 	// logo
-	display.shape_texture(&image_model, adi_gpu::Mat4::new()
-		.translate(0.5, 0.5, 0.0), logo_tex, image_texcoords,
-		false, false, true, true);
+//	display.shape_texture(&image_model, adi_gpu::Mat4::new()
+//		.translate(0.5, 0.5, 0.0), logo_tex, image_texcoords,
+//		false, false, true, true);
 	// triangle
 	let tri = display.shape_gradient(&triangle_model, adi_gpu::Mat4::new(),
 		gradient, false, false, true, true);
 	// new!
-	display.shape_faded(&image_model, adi_gpu::Mat4::new()
-		.translate(1.0, 1.0, 0.0), image_tex, image_texcoords,
-		0.5, false, true, true);
+//	display.shape_faded(&image_model, adi_gpu::Mat4::new()
+//		.translate(1.0, 1.0, 0.0), image_tex, image_texcoords,
+//		0.5, false, true, true);
 	// new!
-	display.shape_tinted(&image_model, adi_gpu::Mat4::new()
-		.translate(1.5, 1.5, 0.0), image_tex, image_texcoords,
-		[1.0, 1.0, 0.0, 1.0], false, false, true, true);
+//	display.shape_tinted(&image_model, adi_gpu::Mat4::new()
+//		.translate(1.5, 1.5, 0.0), image_tex, image_texcoords,
+//		[1.0, 1.0, 0.0, 1.0], false, false, true, true);
 	// new!
-	display.shape_complex(&image_model, adi_gpu::Mat4::new()
-		.translate(1.0, 1.5, 0.0), image_tex, image_texcoords,
-		complexity, false, false, true, true);
+//	display.shape_complex(&image_model, adi_gpu::Mat4::new()
+//		.translate(1.0, 1.5, 0.0), image_tex, image_texcoords,
+//		complexity, false, false, true, true);
 
 	tri
 }
 
 fn main() {
-	let mut window = awi::Window::new("ADI_GPU Animated Example",
-		aci_png::decode(include_bytes!("../res/icon.png")).unwrap());
-	let mut display = adi_gpu::Display::new(&window).unwrap();
+	let mut display = adi_gpu::Display::new("ADI_GPU Animated Example",
+		&aci_png::decode(include_bytes!("../res/icon.png")).unwrap())
+			.unwrap();
 
 	display.color((0.25, 0.25, 1.0));
-	display.fog(Some((20.0, 10.0)));
+//	display.fog(Some((20.0, 10.0)));
 
-	let logo_texture = display.texture(
-		aci_png::decode(include_bytes!("../res/icon.png")).unwrap());
-	let plopgrizzly_texture = display.texture(
-		aci_png::decode(include_bytes!("../res/plopgrizzly.png"))
-			.unwrap());
+//	let logo_texture = display.texture(
+//		aci_png::decode(include_bytes!("../res/icon.png")).unwrap());
+//	let plopgrizzly_texture = display.texture(
+//		aci_png::decode(include_bytes!("../res/plopgrizzly.png"))
+//			.unwrap());
 
-	let mut tri = resize(&mut display, logo_texture, plopgrizzly_texture);
-	let mut ah = 0.0;
+	let mut tri = resize(&mut display, /*logo_texture, plopgrizzly_texture*/);
+//	let mut ah = 0.0;
 
 	'app: loop {
 		// Go through this frame's input.
-		while let Some(input) = window.input() {
+		while let Some(input) = display.input() {
 			use awi::Input::*;
 			use awi::Msg::*;
 
 			match input {
 				Msg(Quit) | Msg(Back) => break 'app,
-				Resize => display.resize(window.wh()),
+//				Resize => {
+//					let wh = display.wh();
+//					display.resize(wh)
+//				},
 				_ => {},
 			}
 		}
 
-		ah += 0.001;
-		display.transform(&mut tri, &adi_gpu::Mat4::new().rotate(0.0, 0.0, ah));
+//		ah += 0.001;
+//		display.transform(&mut tri, &adi_gpu::Mat4::new().rotate(0.0, 0.0, ah));
 
 		display.update();
-		window.update();
+
+//		::std::thread::sleep(::std::time::Duration::from_millis(2000));
+//		break 'app;
 	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,17 @@
 //! Aldaron's Device Interface / GPU is a library developed by Plop Grizzly for
 //! interfacing with the GPU to render graphics or do fast calculations.
 
+extern crate adi_gpu_opengl;
 extern crate adi_gpu_vulkan;
 extern crate adi_gpu_base;
 extern crate afi;
 extern crate ami;
 extern crate awi;
 
+pub use base::Model;
+pub use base::TexCoords;
+pub use base::Gradient;
+pub use base::Shape;
 pub use base::Display as DisplayTrait;
 pub use base::Texture as TextureTrait;
 pub use ami::Mat4;
@@ -21,18 +26,17 @@ use adi_gpu_base as base;
 /// To render anything with adi_gpu, you have to make a `Display`
 pub enum Display {
 	Vulkan(adi_gpu_vulkan::Display),
+	OpenGL(adi_gpu_opengl::Display),
 }
 
 impl DisplayTrait for Display {
-	type Model = Model;
 	type Texture = Texture;
-	type Gradient = Gradient;
-	type TexCoords = TexCoords;
-	type Shape = Shape;
 
-	fn new(window: &awi::Window) -> Option<Self> {
-		if let Some(vulkan) = adi_gpu_vulkan::Display::new(window) {
+	fn new(title: &str, icon: &afi::Graphic) -> Option<Self> {
+		/*if let Some(vulkan) = adi_gpu_vulkan::Display::new(title, icon) {
 			Some(Display::Vulkan(vulkan))
+		} else */if let Some(opengl) = adi_gpu_opengl::Display::new(title, icon) {
+			Some(Display::OpenGL(opengl))
 		} else {
 			None
 		}
@@ -43,12 +47,18 @@ impl DisplayTrait for Display {
 			Display::Vulkan(ref mut display) => {
 				display.color(color)
 			}
+			Display::OpenGL(ref mut display) => {
+				display.color(color)
+			}
 		}
 	}
 
 	fn update(&mut self) {
 		match *self {
 			Display::Vulkan(ref mut display) => {
+				display.update()
+			}
+			Display::OpenGL(ref mut display) => {
 				display.update()
 			}
 		}
@@ -59,13 +69,19 @@ impl DisplayTrait for Display {
 			Display::Vulkan(ref mut display) => {
 				display.camera(xyz, rotate_xyz)
 			}
+			Display::OpenGL(ref mut display) => {
+				display.camera(xyz, rotate_xyz)
+			}
 		}
 	}
 
 	fn model(&mut self, vertices: &[f32], indices: &[u32]) -> Model {
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				Model::Vulkan(display.model(vertices, indices))
+				display.model(vertices, indices)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.model(vertices, indices)
 			}
 		}
 	}
@@ -73,6 +89,9 @@ impl DisplayTrait for Display {
 	fn fog(&mut self, fog: Option<(f32, f32)>) -> () {
 		match *self {
 			Display::Vulkan(ref mut display) => {
+				display.fog(fog)
+			}
+			Display::OpenGL(ref mut display) => {
 				display.fog(fog)
 			}
 		}
@@ -83,13 +102,19 @@ impl DisplayTrait for Display {
 			Display::Vulkan(ref mut display) => {
 				Texture::Vulkan(display.texture(graphic))
 			}
+			Display::OpenGL(ref mut display) => {
+				Texture::OpenGL(display.texture(graphic))
+			}
 		}
 	}
 
 	fn gradient(&mut self, colors: &[f32]) -> Gradient {
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				Gradient::Vulkan(display.gradient(colors))
+				display.gradient(colors)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.gradient(colors)
 			}
 		}
 	}
@@ -97,7 +122,10 @@ impl DisplayTrait for Display {
 	fn texcoords(&mut self, texcoords: &[f32]) -> TexCoords {
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				TexCoords::Vulkan(display.texcoords(texcoords))
+				display.texcoords(texcoords)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.texcoords(texcoords)
 			}
 		}
 	}
@@ -110,6 +138,16 @@ impl DisplayTrait for Display {
 						Texture::Vulkan(ref mut texture) => {
 							texture
 						}
+						_ => panic!("mismatch"),
+					}, pixels)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.set_texture(
+					match *texture {
+						Texture::OpenGL(ref mut texture) => {
+							texture
+						}
+						_ => panic!("mismatch"),
 					}, pixels)
 			}
 		}
@@ -122,14 +160,15 @@ impl DisplayTrait for Display {
 	{
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				Shape::Vulkan(display.shape_solid(
-					match *model {
-						Model::Vulkan(ref model) => {
-							model
-						}
-					},
-					transform, color,
-					blending, fancy, fog, camera)
+				display.shape_solid(
+					model, transform, color,
+					blending, fancy, fog, camera
+				)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.shape_solid(
+					model, transform, color,
+					blending, fancy, fog, camera
 				)
 			}
 		}
@@ -142,19 +181,15 @@ impl DisplayTrait for Display {
 	{
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				Shape::Vulkan(display.shape_gradient(
-					match *model {
-						Model::Vulkan(ref model) => {
-							model
-						}
-					},
-					transform,
-					match colors {
-						Gradient::Vulkan(gradient) => {
-							gradient
-						}
-					},
-					blending, fancy, fog, camera)
+				display.shape_gradient(
+					model, transform, colors,
+					blending, fancy, fog, camera
+				)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.shape_gradient(
+					model, transform, colors,
+					blending, fancy, fog, camera
 				)
 			}
 		}
@@ -167,22 +202,27 @@ impl DisplayTrait for Display {
 	{
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				Shape::Vulkan(display.shape_texture(
-					match *model {
-						Model::Vulkan(ref model) => {
-							model
-						}
-					}, transform,
+				display.shape_texture(
+					model, transform,
 					match texture {
 						Texture::Vulkan(texture) => {
 							texture
 						}
+						_ => panic!("mismatch"),
 					},
-					match tc {
-						TexCoords::Vulkan(texcoords) => {
-							texcoords
+					tc, blending, fancy, fog, camera
+				)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.shape_texture(
+					model, transform,
+					match texture {
+						Texture::OpenGL(texture) => {
+							texture
 						}
-					}, blending, fancy, fog, camera)
+						_ => panic!("mismatch"),
+					},
+					tc, blending, fancy, fog, camera
 				)
 			}
 		}
@@ -195,23 +235,27 @@ impl DisplayTrait for Display {
 	{
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				Shape::Vulkan(display.shape_faded(
-					match *model {
-						Model::Vulkan(ref model) => {
-							model
-						}
-					},
-					transform,
+				display.shape_faded(
+					model, transform,
 					match texture {
 						Texture::Vulkan(texture) => {
 							texture
 						}
+						_ => panic!("mismatch"),
 					},
-					match tc {
-						TexCoords::Vulkan(texcoords) => {
-							texcoords
+					tc, alpha, fancy, fog, camera
+				)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.shape_faded(
+					model, transform,
+					match texture {
+						Texture::OpenGL(texture) => {
+							texture
 						}
-					}, alpha, fancy, fog, camera)
+						_ => panic!("mismatch"),
+					},
+					tc, alpha, fancy, fog, camera
 				)
 			}
 		}
@@ -224,24 +268,29 @@ impl DisplayTrait for Display {
 	{
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				Shape::Vulkan(display.shape_tinted(
-					match *model {
-						Model::Vulkan(ref model) => {
-							model
-						}
-					},
-					transform,
+				display.shape_tinted(
+					model, transform,
 					match texture {
 						Texture::Vulkan(texture) => {
 							texture
 						}
+						_ => panic!("mismatch"),
 					},
-					match tc {
-						TexCoords::Vulkan(texcoords) => {
-							texcoords
+					tc, tint, blending,
+					fancy, fog, camera
+				)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.shape_tinted(
+					model, transform,
+					match texture {
+						Texture::OpenGL(texture) => {
+							texture
 						}
-					}, tint, blending,
-					fancy, fog, camera)
+						_ => panic!("mismatch"),
+					},
+					tc, tint, blending,
+					fancy, fog, camera
 				)
 			}
 		}
@@ -254,41 +303,41 @@ impl DisplayTrait for Display {
 	{
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				Shape::Vulkan(display.shape_complex(
-					match *model {
-						Model::Vulkan(ref model) => {
-							model
-						}
-					}, transform,
+				display.shape_complex(
+					model, transform,
 					match texture {
 						Texture::Vulkan(texture) => {
 							texture
 						}
+						_ => panic!("mismatch"),
 					},
-					match tc {
-						TexCoords::Vulkan(texcoords) => {
-							texcoords
+					tc, tints,
+					blending, fancy, fog, camera
+				)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.shape_complex(
+					model, transform,
+					match texture {
+						Texture::OpenGL(texture) => {
+							texture
 						}
+						_ => panic!("mismatch"),
 					},
-					match tints {
-						Gradient::Vulkan(gradient) => {
-							gradient
-						}
-					},
-					blending, fancy, fog, camera)
+					tc, tints,
+					blending, fancy, fog, camera
 				)
 			}
 		}
 	}
 
-	fn transform(&mut self, shape: &mut Self::Shape, transform: &Mat4) {
+	fn transform(&mut self, shape: &mut Shape, transform: &Mat4) {
 		match *self {
 			Display::Vulkan(ref mut display) => {
-				display.transform(match *shape {
-					Shape::Vulkan(ref mut shape) => {
-						shape
-					}
-				}, transform)
+				display.transform(shape, transform)
+			}
+			Display::OpenGL(ref mut display) => {
+				display.transform(shape, transform)
 			}
 		}
 	}
@@ -298,39 +347,39 @@ impl DisplayTrait for Display {
 			Display::Vulkan(ref mut display) => {
 				display.resize(wh)
 			}
+			Display::OpenGL(ref mut display) => {
+				display.resize(wh)
+			}
+		}
+	}
+
+	fn wh(&self) -> (u32, u32) {
+		match *self {
+			Display::Vulkan(ref display) => {
+				display.wh()
+			}
+			Display::OpenGL(ref display) => {
+				display.wh()
+			}
+		}
+	}
+
+	fn input(&mut self) -> Option<awi::Input> {
+		match *self {
+			Display::Vulkan(ref mut display) => {
+				display.input()
+			}
+			Display::OpenGL(ref mut display) => {
+				display.input()
+			}
 		}
 	}
 }
 
-/// A list of vertices that make a shape.
-#[derive(Copy, Clone)]
-pub enum Model {
-	Vulkan(adi_gpu_vulkan::Model)
-}
-
-impl base::Model for Model { }
-
-/// A list of colors to be paired with vertices.
-#[derive(Copy, Clone)]
-pub enum Gradient {
-	Vulkan(adi_gpu_vulkan::Gradient)
-}
-
-impl base::Gradient for Gradient {
-}
-
-/// A list of texture coordinates to be paired with vertices.
-#[derive(Copy, Clone)]
-pub enum TexCoords {
-	Vulkan(adi_gpu_vulkan::TexCoords)
-}
-
-impl base::TexCoords for TexCoords {
-}
-
 #[derive(Copy, Clone)]
 pub enum Texture {
-	Vulkan(adi_gpu_vulkan::Texture)
+	Vulkan(adi_gpu_vulkan::Texture),
+	OpenGL(adi_gpu_opengl::Texture)
 }
 
 impl base::Texture for Texture {
@@ -340,15 +389,10 @@ impl base::Texture for Texture {
 			Texture::Vulkan(ref texture) => {
 				texture
 			}
+			Texture::OpenGL(ref texture) => {
+				texture
+			}
 		};
 		this.wh()
 	}
-}
-
-/// A renderable object that exists on the `Display`.
-pub enum Shape {
-	Vulkan(adi_gpu_vulkan::Shape)
-}
-
-impl base::Shape for Shape {
 }
